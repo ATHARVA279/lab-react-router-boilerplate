d0858414c730b459bff6d891f13f24a09eba6bb7
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-01-24 14:11:13 +0530

    added

---
+++ router/src/Components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      <nav>
+        <h1>Kalvium❤️</h1>
+        <div className='flex'>
+          <h3>Contact</h3>
+          <h3>About</h3>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
